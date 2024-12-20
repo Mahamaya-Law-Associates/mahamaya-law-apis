bff97120f1e0e7632242e1b0f193727a908bfd74
nikk-16 <nikhilchaurasiya16401@gmail.com>
2024-12-21 00:25:05 +0530

    update config

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const port = 4200;
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'http://localhost:3000',
-    origin: 'https://mahamaya-law.in'
+    origin: [ 'http://localhost:3000', 'https://mahamaya-law.in', 'https://mahamaya-law.vercel.app/', 'https://mahamaya-law.vercel.app' ]
 }));
 
 app.use(express.json())
