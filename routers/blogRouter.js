2b4a73ebec6d8755fc33984d3e83a84604d419d6
nikk-16 <nikhilchaurasiya16401@gmail.com>
2024-12-21 23:17:52 +0530

    update router

--- routers/blogRouter.js
+++ routers/blogRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = require('../index.js');
 const verifyToken = require('./verifytoken');
 const Model  = require('../models/blogModel');
 require('dotenv').config();
