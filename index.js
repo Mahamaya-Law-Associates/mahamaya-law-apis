cd4ec817bef0c48972f95e4826af5f78306b8da7
nikk-16 <nikhilchaurasiya16401@gmail.com>
2025-02-02 04:25:19 +0530

    update auth

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use('/blog', blogRouter);
 
 router.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
 });
 
